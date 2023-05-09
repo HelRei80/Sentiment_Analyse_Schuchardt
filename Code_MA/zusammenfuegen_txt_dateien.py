94804cf2d43eda260032f13fe2c459fd3a8ebe9a
HelRei80 <83943693+HelRei80@users.noreply.github.com>
2023-05-09 21:06:40 +0200

    Update zusammenfuegen_txt_dateien.py

--- Code_MA/zusammenfuegen_txt_dateien.py
+++ Code_MA/zusammenfuegen_txt_dateien.py
@@ -1,11 +1,12 @@
-import os
-import bs4         
-import lxml        
-from bs4 import BeautifulSoup as bs
+# es werden norwendige Pakete importiert:
 
+import os
+
+# der Pfad zu den Dateien wird definiert:
 pfad = r"pfad\zu\dateien\\"
 os.chdir(pfad)
 
+# es wird über die Dateien iteriert, die Dateien werden  eingelesen und anschließend in einer anderen Datein angehängt:
 for datei in os.listdir(pfad): 
     f = os.path.join(pfad, datei) 
     if os.path.isfile(f): 
